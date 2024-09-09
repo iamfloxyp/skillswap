f7fdd6ce72426eaa0484b3674416c6ccc1884369
floxyp <onyekao163@gmail.com>
2024-09-09 11:16:42 -0400

    Feat:Add the newsletter and Footer section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Navbar toggling
 const menuIcon = document.getElementById('menu-icon');
 const closeIcon = document.getElementById('close-icon');
 const navLinks = document.querySelector(".nav-links");
@@ -12,3 +13,21 @@ closeIcon.addEventListener('click',()=>{
     menuIcon.style.display ="block";
     closeIcon.style.display= 'none';
 });
+// Faq section toggling
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+    question.classList.toggle('show-text');
+  });
+});
+
+// year
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
