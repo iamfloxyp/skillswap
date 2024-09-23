b1fe307619f3340701c2cea23a660c5b92faa180
floxyp <onyekao163@gmail.com>
2024-09-23 03:23:11 -0400

    Feat: Add the sign up and login page

--- index.js
+++ index.js
@@ -31,3 +31,19 @@ questions.forEach(function (question) {
 // year
 const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
+
+// toggle button for the password
+function togglePassword(inputId) {
+  const input = document.getElementById(inputId);
+  const icon = input.nextElementSibling.querySelector('i');
+
+  if (input.type === 'password') {
+      input.type = 'text';
+      icon.classList.remove('fa-eye-slash');
+      icon.classList.add('fa-eye');
+  } else {
+      input.type = 'password';
+      icon.classList.remove('fa-eye');
+      icon.classList.add('fa-eye-slash');
+  }
+}
